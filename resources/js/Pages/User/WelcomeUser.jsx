54efb6813e2228edbc866f4ca6df59abc99d5f5d
Gregory Sanchez <mcgregox@gmail.com>
2024-07-22 14:11:55 -0300

    Laravel11 Jetstream Inertia React (tsx and jsx) template

---
+++ resources/js/Pages/User/WelcomeUser.jsx
@@ -0,0 +1,7 @@
+export default function WelcomeUser() {
+    return (
+        <div className="p-6 sm:px-20 bg-white border-b border-gray-200">
+            <div className="mt-8 text-6xl text-gray-500">Welcome user</div>
+        </div>
+    );
+}
---
+++ vite.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [
+    laravel({
+      input: 'resources/js/app.tsx',
+      refresh: true,
+    }),
+    react(),
+  ],
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', 'jsx'],
+    alias: {
+      '@': '/resources/js',
+    },
+  },
+});
